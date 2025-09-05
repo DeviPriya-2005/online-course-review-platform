1cea526ed86801df7e13a3b205fea4fcb26089e7
DEVI PRIYA C <devipriyachandirakumar@gmail.com>
2025-09-05 09:26:15 +0530

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+    private String title;
+    private String category;
+    private String instructor;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+
+    // Getters & Setters
+}
